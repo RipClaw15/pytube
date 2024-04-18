6cdd2ada82edaa90c400641b9114259259d01edc
RipClaw15 <csongor1551@gmail.com>
2024-04-18 22:16:00 +0300

    playlist download functioning as intended

--- youtube_downloader/downloader/views.py
+++ youtube_downloader/downloader/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, StreamingHttpResponse
 from pytube import YouTube, Playlist
 import os
+import zipfile
+import io
 
 # Create your views here.
 def index(request):
@@ -53,38 +55,37 @@ def playlist_download(request):
         link = request.GET.get('link')
         media_type = request.GET.get('media_type')
         if link:
-
             try:
                 p = Playlist(link)
                 print(f'Downloading: {p.title}')
-                if media_type == 'audio': 
-                    
-                    for video in p.videos:
-                    
-                        audio_stream = video.streams.filter(only_audio=True).first().download()
-                    content_type = 'audio/mpeg' 
-                elif media_type == 'video':
-                    for video in p.videos:
-                        video_stream = video.streams.filter(resolution='720p', file_extension='mp4').first().download()
-                    content_type = 'video/mp4' 
-                else:
+
+                zip_filename = f'{p.title}_download.zip'
+
+                in_memory_zip = io.BytesIO()
+                with zipfile.ZipFile(in_memory_zip, 'w') as zipf:
+                    if media_type == 'audio':
+                        for video in p.videos:
+                            audio_stream = video.streams.filter(only_audio=True).first().download()
+                            zipf.write(audio_stream, arcname=f'{video.title}.mp3')
+                            os.remove(audio_stream)
+                    elif media_type == 'video':
+                        for video in p.videos:
+                            video_stream = video.streams.filter(resolution='720p', file_extension='mp4').first().download()
+                            zipf.write(video_stream, arcname=f'{video.title}.mp4')
+                            os.remove(video_stream)
+                    else:
                         raise ValueError("Invalid media type")
 
-                
-                
-                response = HttpResponse(content_type=content_type)
-    
-                
+                in_memory_zip.seek(0)
+                response = HttpResponse(in_memory_zip, content_type='application/zip')
+                response['Content-Disposition'] = f'attachment; filename="{zip_filename}"'
                 return response
-
             except Exception as e:
                 return HttpResponse(f"An error occurred: {e}", status=500)
-                
         else: 
             return HttpResponse("No link provided", status=400)
     else:
         return HttpResponse("Method not allowed", status=405)
-                
 
 def channel_download(request):
     pass
